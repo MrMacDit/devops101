d35bf7357b349cdf4dc487e90c67b9b929505ec5
MrMacDit <72633529+MrMacDit@users.noreply.github.com>
2023-07-18 05:26:50 +0100

    linux_params and ubuntu_params should have been .tfvars

--- Task3/dev_devop/linux_params.tf
+++
@@ -1,6 +0,0 @@
-ec2_ami           = "ami-0fb2f0b847d44d4f0"
-ec2_instance_type = "t2.micro"
-ec2_key_name      = "lesson1"
-user_names        = ["ubuntu_Dev_machine", "ubuntu_SIT_machine", "ubuntu_Prod_machine"]
-ec2_environ       = ["Developmental", "SIT", "Production"]
-ec2_descript      = ["This machine is majorly for testing", "Only deploy to this machine once your code is stable and requires further testing", "Only deploy welltested and stable codes to this Machine"]
